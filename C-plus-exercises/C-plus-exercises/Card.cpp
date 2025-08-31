6395fd28f386b3524c05b4c3665aac2eb72457e3
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2025-08-31 15:32:38 +0900

    up

---
+++ C-plus-exercises/C-plus-exercises/Card.cpp
@@ -0,0 +1,12 @@
+﻿#include "Card.h"
+
+// コンストラクタ
+Card::Card(std::string s, int r) : suit(s), rank(r) {}
+
+// toStringメソッド
+std::string Card::toString() const {
+    static const std::string rankNames[14] =
+    { "", "A", "2", "3", "4", "5", "6", "7",
+     "8", "9", "10", "J", "Q", "K" };
+    return suit + rankNames[rank];
+}
